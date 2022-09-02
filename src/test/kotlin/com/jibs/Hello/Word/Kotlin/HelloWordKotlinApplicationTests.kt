42a9c747e8389698596f030f7a6d64dd89a1dc20
joelma <joelmadev@gmail.com>
2022-09-02 18:48:09 -0300

    ABC/01-Projeto inicial

---
+++ src/test/kotlin/com/jibs/Hello/Word/Kotlin/HelloWordKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jibs.Hello.Word.Kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloWordKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
