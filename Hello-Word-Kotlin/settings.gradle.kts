87d7c7f840030802f130876f821e5a71a9cfca0f
Joelma Bittencourt <83016532+JoelmaBittencourt@users.noreply.github.com>
2022-09-02 18:55:45 -0300

    Add files via upload

---
+++ Hello-Word-Kotlin/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Hello-Word-Kotlin"
---
+++ Hello-Word-Kotlin/src/main/kotlin/com/jibs/Hello/Word/Kotlin/HelloWordKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.jibs.Hello.Word.Kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloWordKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelloWordKotlinApplication>(*args)
+}
