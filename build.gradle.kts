457612a72cf2612d99ed7c9a88733ad74a9698cb
joelma <joelmadev@gmail.com>
2022-09-13 08:26:11 -0300

    JIB/01-Criando interação de java com kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,21 +14,29 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 repositories {
 	mavenCentral()
 }
+// sourceSets{
+//	main {
+//		java.srcDirs("scr/main/kotlin")
+//	}
+//sourceSets {
+//	main.java.srcDirs += "scr/main/kotlin"
+//}
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
 	}
-}
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "17"
+		}
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
--- src/main/kotlin/com/jibs/Hello/Word/Kotlin/HelloWordKotlinApplication.kt
+++ src/main/kotlin/com/jibs/Hello/Word/Kotlin/HelloWordKotlinApplication.kt
@@ -2,15 +2,14 @@ package com.jibs.Hello.Word.Kotlin
 
 import CarManufacturer
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class HelloWordKotlinApplication
 
 fun main(args: Array<String>) {
 	println("Hello word")
-	val student= Student("Alex")
-	println("Studant name:${student.name}")
-	val car= CarManufacturer("Mercedes")
-	println("Car name:${car.name}")
+//	val student= Student("Alex")
+//	println("Studant name:${student.name}")
+//	val car= CarManufacturer("Mercedes")
+//	println("Car name:${car.name}")
 }
